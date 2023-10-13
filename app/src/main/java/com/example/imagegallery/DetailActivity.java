d9c6a4345069d07a3348bf37487405191bbc7e18
chanhuy <luuchanhuy142@gmail.com>
2023-10-13 09:26:44 +0700

    Added popup menu for set wallpaper

--- app/src/main/java/com/example/imagegallery/DetailActivity.java
+++ app/src/main/java/com/example/imagegallery/DetailActivity.java
@@ -1,13 +1,20 @@
 package com.example.imagegallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
 import android.app.WallpaperManager;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+
+import java.io.File;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -23,6 +30,26 @@ public class DetailActivity extends AppCompatActivity {
         Button btnSetWallpaper = findViewById(R.id.btnSetWallpaper);
         btnSetWallpaper.setOnClickListener(v -> {
             Wallpapersetter.setWallpaper(this, obj.getFilePath());
+        });
+
+        ImageView iv_more = findViewById(R.id.iv_more);
+        iv_more.setOnClickListener(v -> {
+            PopupMenu popupMenu = new PopupMenu(this, iv_more);
+            popupMenu.getMenuInflater().inflate(R.menu.detail_image_popup, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(item -> {
+                int itemId = item.getItemId();
+                if(R.id.set_wallpaper == itemId){
+                    Toast.makeText(this, "Set wallpaper", Toast.LENGTH_SHORT).show();
+                    Wallpapersetter.setWallpaper(this, obj.getFilePath());
+                }
+                else if(R.id.set_lockscreen == itemId){
+                    Toast.makeText(this, "Set lockscreen", Toast.LENGTH_SHORT).show();
+                    Wallpapersetter.setLockScreen(this, obj.getFilePath());
+                }
+                return true;
+            });
+            //shơw popup menu
+            popupMenu.show();
         });
 
 
