a1f54603736bb29317c170b57af5f281fc85807a
JackShinigami <quangkhanh21112003@gmail.com>
2023-10-13 14:23:02 +0700

    Add SharedPreferences Data Manager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,5 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
